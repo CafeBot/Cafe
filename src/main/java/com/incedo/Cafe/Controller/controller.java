0f48c76259e9fccdb408c2045f54899e12803180
CafeBot <53302634+CafeBot@users.noreply.github.com>
2019-07-30 20:43:43 +0530

    paytm integration (#6)
    * paytm integration

--- src/main/java/com/incedo/Cafe/Controller/controller.java
+++ src/main/java/com/incedo/Cafe/Controller/controller.java
@@ -2,7 +2,10 @@ package com.incedo.Cafe.Controller;
 
 import com.incedo.Cafe.Greeting;
 import com.incedo.Cafe.Pojo.Cart;
+import com.incedo.Cafe.Services.Services;
 import com.incedo.Cafe.Services.cartService;
+import com.incedo.Cafe.Services.paytmService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +15,46 @@ import org.springframework.web.bind.annotation.*;
 public class controller {
 
 
-    @PostMapping("/www.cafe.com/user/order/pay")
+    @PostMapping("/user/order/save")
     public String saveOrder(@RequestBody Cart carts){
         cartService cartService = new cartService();
         cartService.saveOrder(carts);
         return "Success";
+    }
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+/*
+    @PostMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        Greeting g = new Greeting(1,"akash");
+        return g;
+    }
+    @PostMapping("/abc")
+    public void hello(@RequestBody Greeting g){
+        System.out.println(g.getContent()+" "+g.getId());
+        jdbcTemplate.execute("insert into user_details values(1,'alkash',98728327)");
+
+    }
+    */
+
+
+    @GetMapping("/user/order/pay")
+    public Object paytm(){
+        paytmService paytmService = new paytmService();
+        return paytmService.paytm();
+    }
+
+    @PostMapping("/paytmStatus")
+    public Object Satus() {
+        return "response";
+    }
+
+    @ExceptionHandler
+    @PostMapping
+    public Object error() {
+        return "Fail";
     }
 
 
