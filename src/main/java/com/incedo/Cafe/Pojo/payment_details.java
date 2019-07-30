adee3e89add94e202d1325235eeac18f3a150495
CafeBot <53302634+CafeBot@users.noreply.github.com>
2019-07-30 19:34:07 +0530

    adding pojo  (#4)
    adding pojos and API for orderDetails

---
+++ src/main/java/com/incedo/Cafe/Pojo/payment_details.java
@@ -0,0 +1,4 @@
+package com.incedo.Cafe.Pojo;
+
+public class payment_details {
+}
---
+++ src/main/java/com/incedo/Cafe/Repository/CartRepo.java
@@ -0,0 +1,38 @@
+package com.incedo.Cafe.Repository;
+
+import com.incedo.Cafe.Pojo.Cart;
+import com.incedo.Cafe.Pojo.Snack;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CartRepo {
+    @Autowired
+    CreateJdbcTemplate createJdbcTemplate = new CreateJdbcTemplate();
+    JdbcTemplate jdbcTemplate;
+
+    {
+        try {
+            jdbcTemplate = createJdbcTemplate.CreateTemplate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    ;
+
+
+
+    public void insert_details(Cart cart) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String strDate= formatter.format( cart.getDatetime());
+        System.out.println("date="+cart.getDatetime()+"  str="+strDate);
+        for (Snack snack: cart.getSnack()) {
+            jdbcTemplate.update("insert into cart values(" + cart.getEmp_id() + "," + cart.getCart_id() + "," + snack.getSnack_id() + "," + snack.getQty() + ",'" + strDate + "'," + cart.getTotal() + "," + cart.isPayment_status() + ")");
+        }
+    }
+
+}
