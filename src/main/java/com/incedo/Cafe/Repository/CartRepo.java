632b5ef8def8a4bbe39cd1a04bf5c02db0e00f16
rushal bhalerao <rushal.bhalerao@incedoinc.com>
2019-07-30 17:20:04 +0530

    tomcat

--- src/main/java/com/incedo/Cafe/Repository/CartRepo.java
+++ src/main/java/com/incedo/Cafe/Repository/CartRepo.java
@@ -27,13 +27,10 @@ public class CartRepo {
 
 
     public void insert_details(Cart cart) {
-    //  jdbcTemplate.execute("insert into cart values(3,8,9,45,'2016-01-01',456,true)");
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         String strDate= formatter.format( cart.getDatetime());
         System.out.println("date="+cart.getDatetime()+"  str="+strDate);
         for (Snack snack: cart.getSnack()) {
-
-
             jdbcTemplate.update("insert into cart values(" + cart.getEmp_id() + "," + cart.getCart_id() + "," + snack.getSnack_id() + "," + snack.getQty() + ",'" + strDate + "'," + cart.getTotal() + "," + cart.isPayment_status() + ")");
         }
     }
