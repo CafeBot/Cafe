8b0ac38363208750e1778e33d9cff2bdc84464b9
rushal bhalerao <rushal.bhalerao@incedoinc.com>
2019-07-30 17:20:04 +0530

    tomcat

--- src/main/java/com/incedo/Cafe/Services/cartService.java
+++ src/main/java/com/incedo/Cafe/Services/cartService.java
@@ -11,21 +11,8 @@ public class cartService {
 
     public void saveOrder(Cart cart) {
 
-        CartRepo crp=new CartRepo();
+        CartRepo crp = new CartRepo();
         crp.insert_details(cart);
-
-         //   String query1 = "insert into cart values("+cart.getEmp_id()+","+cart.getCart_id()+","+cart.getSnack_id()+","+cart.getQty()+",'"+cart.getDatetime()+"',"+cart.getTotal()+","+cart.isPayment_status()+")";
-
-          //  for (Snack snack: cart.getSnack()
-             //    )
-            //    {
-         //       System.out.println("cart_id"+cart.getCart_id()+"  Payment Status="+cart.isPayment_status()+" datetime="+cart.getDatetime()+" snack:"+snack.getSnack_id());
-              // jdbcTemplate.update( "insert into cart values("+cart.getEmp_id()+","+cart.getCart_id()+","+snack.getSnack_id()+","+snack.getQty()+",'"+cart.getDatetime()+"',"+cart.getTotal()+","+cart.isPayment_status()+")");
-                //jdbcTemplate.update("insert into cart (emp_id,cart_id)values(??)",cart.getEmp_id(),cart.getCart_id());
-
-            }
-
-
-
     }
+}
 
