be0b8ee6c964fa2ee974a36afc7c0b5a747b3d2f
Akash Barne <akash.barne@incedoinc.com>
2019-07-31 17:57:27 +0530

    Paytm Integration

--- src/main/java/com/incedo/Cafe/Controller/controller.java
+++ src/main/java/com/incedo/Cafe/Controller/controller.java
@@ -1,5 +1,6 @@
 package com.incedo.Cafe.Controller;
 import com.incedo.Cafe.Pojo.Cart;
+import com.incedo.Cafe.Pojo.Paytm;
 import com.incedo.Cafe.Services.cartService;
 import com.incedo.Cafe.Services.paytmService;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +11,11 @@ public class controller {
 
 
     @PostMapping("/user/order/save")
-    public Object saveOrder(@RequestBody Cart carts){
+    public int saveOrder(@RequestBody Cart carts){
         //Cart carts = (Cart) object_cart;
         cartService cartService = new cartService();
         int Cart_id = cartService.saveOrder(carts);
-        return paytm(carts.getEmp_id(),Cart_id,carts.getTotal(),carts.getPh_no());
+        return Cart_id;
     }
 
 
@@ -25,21 +26,15 @@ public class controller {
         return "Success";
     }
 
-//    localhost:8080/user/order/pay?Emp_id=11111&Cart_id=11111&Total=100&Ph_no=8237668870
-//    @GetMapping(value ="/user/order/pay" ,params = {"Emp_id","Cart_id","Total","Ph_no"})
-//    public Object paytmdirect(@RequestParam Integer Emp_id,@RequestParam Integer Cart_id,@RequestParam Float Total,@RequestParam Long Ph_no ){
-//        paytmService paytmService = new paytmService();
-//        return paytmService.paytm(Emp_id,Cart_id,Total,Ph_no);
-//    }
+    //localhost:8080/user/order/pay?Emp_id=11111&Cart_id=11111&Total=100&Ph_no=8237668870
+    //@GetMapping(value = "/user/order/pay", params = {"emp_id","cart_id","total","ph_no"})
+    //public Object paytm(@RequestParam int emp_id,@RequestParam int cart_id,@RequestParam int total,@RequestParam long ph_no){
 
-
-
-
-
-
-    public Object paytm(int Emp_id,int Cart_id,float Total,long Ph_no ){
+    @PostMapping("/user/order/pay")
+    public Object paytm(@RequestBody Paytm paytm){
         paytmService paytmService = new paytmService();
-        return paytmService.paytm(Emp_id,Cart_id,Total,Ph_no);
+        //Paytm paytm = new Paytm(emp_id,cart_id,total,ph_no);
+        return paytmService.paytm(paytm);
     }
 
     @PostMapping("/paytmStatus")
